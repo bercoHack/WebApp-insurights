bfc7ab93b9a1862576ddce90964247b96f754d48
bercoHack <guy.berco03@gmail.com>
2021-12-05 17:05:04 +0200

    Update app.py

--- FlaskTry/MoviesApp/web-app/app.py
+++ FlaskTry/MoviesApp/web-app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, session, Response
 from werkzeug.utils import redirect
 from flask_cors import CORS, cross_origin
+import pathlib
 import os
 import json
 
@@ -12,7 +13,8 @@ isLoggedIn = False
 @app.route('/api/movies')
 @cross_origin(supports_credentials=True)
 def getMovies():
-    f = open("C:\\Users\\User\\Documents\\GitHub\\WebApp-insurights\\FlaskTry\\MoviesApp\\\web-app\\db.json", "r")
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    f = open(str(pathlib.Path(__file__).parent.resolve())+ "\\db.json", "r")
     data_dict = json.loads(f.read())
     return json.dumps(data_dict["movies"])
 
